31e2c6de2b3f103aff7dd74abe5a8194fc2cb84f
Matheus <matheus.afornali@gobrax.com>
2020-01-26 10:41:46 -0300

    - Finalizando interface web do DevRadar - Semana Omnistack. Signed-off-by: Matheus <afornalimatheus@gmail.com>

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 // UsetState -> função usada pelo React para informar o estado.
 // Componente: bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
@@ -11,72 +15,35 @@ import './Main.css'
 // Estado: informações mantidas pelo componente (Lembrar: imutabilidade).
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      
+      setDevs(response.data);
+    }
+    
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
   
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div class="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/23086440?v=4" alt="" />
-              <div className="user-info">
-                <strong>Matheus Afornali</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Developer passionate about vintage cars.</p>
-            <a href="https://github.com/afornalimatheus">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/23086440?v=4" alt="" />
-              <div className="user-info">
-                <strong>Matheus Afornali</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Developer passionate about vintage cars.</p>
-            <a href="https://github.com/afornalimatheus">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/23086440?v=4" alt="" />
-              <div className="user-info">
-                <strong>Matheus Afornali</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Developer passionate about vintage cars.</p>
-            <a href="https://github.com/afornalimatheus">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul> 
       </main>
     </div>
